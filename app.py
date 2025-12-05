8f3a340b712eb4b58eb620d6478e4d709b2ab3d5
Lolors <rolilin8@gmail.com>
2025-12-05 13:58:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1260,8 +1260,6 @@ def render_tab_move():
         stock_loc_display = stock_summary_text
     else:
         stock_loc_display = current_zone
-    else:
-        stock_loc_display = current_zone
 
     col_left2, col_right2 = st.columns(2)
 
